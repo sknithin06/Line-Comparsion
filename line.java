c2bacecc8397e81fc6543e82cad90b704b01659b
sknithin06 <nithinsk939@gmail.com>
2021-07-07 22:23:17 +0530

    checking the compare of point

--- line.java
+++ line.java
@@ -17,6 +17,21 @@ public class line {
 		else
 			System.out.println("Lines are not identical");
 	}
+	void compareLength(double x1, double x2, double y1, double y2, double m1, double m2, double n1, double n2) {
+		double g1 = (y2 - y1) * m1 + (x1 - x2) * n1
+	            + (x2 * y1 - x1 * y2);
+		double g2 = (y2 - y1) * m2 + (x1 - x2) * n2
+	            + (x2 * y1 - x1 * y2);;
+	     if (Double.compare(g1, g2) == 0) {
+	    	 System.out.println("Both lines are equal");
+	     }
+	     else if (Double.compare(g1, g2) < 0) {        
+	    	 System.out.println("One line is lesser than another line");
+	     }
+	     else {
+	    	 System.out.println("One is greater than another line");
+	     }
+	}
 	
 	public static void main(String[] args) {
 	System.out.println("Welcome to Line Comparsion");
@@ -44,8 +59,18 @@ public class line {
 	System.out.print("Enter sixth number:");  
 	double c2=sc.nextInt(); 
 	
+	System.out.print("Enter first number- ");  
+	double m1=sc.nextInt();  
+	System.out.print("Enter second number- ");  
+	double m2=sc.nextInt();  
+	System.out.print("Enter third number- ");  
+	double n1= sc.nextInt();  
+	System.out.print("enter fourth number-");
+	double n2= sc.nextInt();
+	
 	line point = new line();
 	point.distance(x1, x2, y1, y2);
 	point.checkEquality(a1, a2, b1, b2, c1, c2);
+	point.compareLength(x1, x2, y1, y2, m1, m2, n1, n2);
 	}
 }
